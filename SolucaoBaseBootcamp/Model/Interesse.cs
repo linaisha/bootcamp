bf5255f548dd66f79705abf64eddd3ba14035739
Elisa de Oliveira Martins <62621575+linaisha@users.noreply.github.com>
2021-05-01 18:07:08 -0300

    Delete SolucaoBaseBootcamp directory

--- SolucaoBaseBootcamp/Model/Interesse.cs
+++
@@ -1,9 +0,0 @@
-namespace SolucaoBaseBootcamp.Model
-{
-    public class Interesse
-    {
-    public string nome { get; set; }
-    public int id { get; set; }
-    public string email { get; set; }
-    }
-}
